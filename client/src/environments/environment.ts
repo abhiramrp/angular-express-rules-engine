3f4f80605703658f57079309eb80ccd228cb65f5
Hrushikesh98d <hdandge@uncc.edu>
2024-12-04 18:12:18 -0500

    Added the Envi

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  cognitoClientId: '45hrc14o258efargko3bn6tnpk',
+  cognitoDomain: 'hrushikesh.auth.us-east-2.amazoncognito.com',
+  redirectUri: 'http://localhost:4200'
+};
